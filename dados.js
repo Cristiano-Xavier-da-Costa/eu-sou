aebd885a5dc846fb0e0b5ad41409878e99953c02
Cristiano-Xavier-da-Costa <cristiano.43748910@educa.rj.gov.br>
2024-09-08 17:42:23 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+{
+    titulo: "Afirmação 1: \"EU SOU\" a Ressurreição e a Vida.",
+    interpretacao: "Essa afirmação representa a natureza divina de Jesus Cristo como fonte de vida eterna e renovação espiritual.",
+    link: "https://grandefraternidadebranca.com.br/eusou_afirmacoes.htm",
+    tags: "jesus cristo ressurreicao vida divina espiritualidade fé"
+  },
+  {
+    titulo: "Afirmação 2: \"EU SOU\" o Caminho, a Verdade e a Vida.",
+    interpretacao: "Jesus se apresenta como o único caminho para a salvação, a verdade absoluta e a fonte de vida.",
+    link: "https://grandefraternidadebranca.com.br/eusou_afirmacoes.htm",
+    tags: "jesus cristo caminho verdade vida salvacao absoluto"
+  },
+  {
+    titulo: "Afirmação 3: \"EU SOU\" a luz do mundo; o que me segue não anda nas trevas, mas terá a luz da vida.",
+    interpretacao: "Jesus se autodenomina a luz que ilumina o mundo, oferecendo orientação e esperança para aqueles que o seguem.",
+    link: "https://grandefraternidadebranca.com.br/eusou_afirmacoes.htm",
+    tags: "jesus cristo luz mundo escuridão orientação esperança"
+  },
+  {
+    titulo: "Afirmação 4: \"EU SOU\" a Porta Aberta que nenhum homem pode fechar.",
+    interpretacao: "Jesus se apresenta como a única porta de acesso a Deus, enfatizando sua autoridade e exclusividade.",
+    link: "https://grandefraternidadebranca.com.br/eusou_afirmacoes.htm",
+    tags: "jesus cristo porta acesso deus autoridade exclusividade"
+  },
+  {
+    titulo: "Afirmação 5: \"EU SOU\" a grande Opulência de Deus feita visível para meu uso correto, agora e sempre.",
+    interpretacao: "Essa afirmação destaca a abundância e a provisão divina, disponíveis para todos aqueles que confiam em Deus.",
+    link: "https://grandefraternidadebranca.com.br/eusou_afirmacoes.htm",
+    tags: "deus opulencia abundancia provisão fé espiritualidade"
+  }
+];
